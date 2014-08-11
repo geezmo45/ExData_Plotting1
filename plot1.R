d73848134b2f0ba293c1162acb019e5f41144813
geezmo45 <geezmo45@gmail.com>
2014-08-11 02:49:27 +0200

    fixed read.table parameters

--- plot1.R
+++ plot1.R
@@ -4,7 +4,7 @@
 # if file stored remotely
 temp <- tempfile()
 download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
-x <- read.table(unz(temp, "household_power_consumption.txt"))
+x <- read.table(unz(temp, "household_power_consumption.txt", header=TRUE, sep=";"))
 unlink(temp)
 
 # make sure date column is in the proper format
